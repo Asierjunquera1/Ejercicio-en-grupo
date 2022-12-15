d9d8aea504cb795cf966378a663a8cebc007c280
Asierjunquera1 <ajunquerabartolome@gmail.com>
2022-12-15 16:15:28 +0100

    a

---
+++ compararproblemas.py
@@ -0,0 +1,9 @@
+def comparar(a, b):
+    puntuacion_lucia=0
+    puntuacon_carlos=0
+    for i in range 5:
+        if a[i]>b[i]:
+            puntuacion_lucia+=1
+        elif a[i]<b[i]:
+            puntuacion_carlos+=1
+        
