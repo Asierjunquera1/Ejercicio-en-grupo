c0bcfa1140a8eb064f9143f9a2e6f025061db3e2
Asierjunquera1 <ajunquerabartolome@gmail.com>
2022-12-15 16:23:52 +0100

    aa

--- compararproblemas.py
+++ compararproblemas.py
@@ -1,9 +1,36 @@
-def comparar(a, b):
+
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+# 1. INTEGER_ARRAY a
+# 2. INTEGER_ARRAY b
+#
+def compareTriplets(a, b):
     puntuacion_lucia=0
-    puntuacon_carlos=0
-    for i in range 5:
+    puntuacion_carlos=0
+    for i in range(len(a)):
         if a[i]>b[i]:
             puntuacion_lucia+=1
         elif a[i]<b[i]:
-            puntuacion_carlos+=1
+            puntuacion_carlos+=1 
+    lista=[puntuacion_lucia, puntuacion_carlos]
+    return lista
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+    result = compareTriplets(a, b)
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
+
         
